a86c996618792d7eb87438a0fb720d5daac8ca1f
Minilik Shumete <minilikshumete@gmail.com>
2023-09-27 01:45:53 +0300

    check env and invoked sequensially

---
+++ config/a_check_env.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+)
+var (
+	envs []string
+	messages []string
+)
+
+  func init(){
+	godotenv.Load()
+	envs = []string{"PORT"}
+	// os.LookupEnv("key")
+	for _, val := range  envs {
+		if el, exist:= os.LookupEnv(val); (!exist || el == "") {
+			// env exist but its value is empty or it's not exist at all.
+			// m := fmt.Sprintf(`Error encountered when checking env vars ' %s ' not found`, val)
+			messages = append(messages,val )
+		}
+	}
+
+	if len(messages) > 0 {
+		logrus.Errorf("Error encountered when checking those env vars ->>>>>> %v", strings.Join(messages, ", "))
+		panic("Error encountered when checking environmental variables")
+	}
+}
