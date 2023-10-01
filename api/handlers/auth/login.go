9b39c41ed9fcd083332bb9896b1956c407efc3e3
Minilik Shumete <minilikshumete@gmail.com>
2023-10-02 01:43:28 +0300

    login and signup handler including 2FA auth

--- api/handlers/auth/login.go
+++ api/handlers/auth/login.go
@@ -1,33 +1,75 @@
 package auth
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+	"strings"
 
+	mutations "github.com/Ethiopian-Education/edu-auth-server.git/api/handlers/mutation"
+	"github.com/Ethiopian-Education/edu-auth-server.git/api/handlers/queries"
 	jwt_jwt "github.com/Ethiopian-Education/edu-auth-server.git/crypto/jwt"
 	"github.com/Ethiopian-Education/edu-auth-server.git/model"
-	"github.com/Ethiopian-Education/edu-auth-server.git/utils/services"
+	"github.com/Ethiopian-Education/edu-auth-server.git/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
 
 func LoginHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-
+//  CURRENTLY WE USE PHONE_NUMBER TO LOGIN... IN THE RECENT FUTURE WE'LL UPGRADE THIS TO USE USERNAME, PHONE_NUMBER OR EMAIL AS USERS PREFERENCE
 		var err error
 
-		var claims = &jwt_jwt.JWTClaims{}
-		metadata := map[string]interface{}{
-			"roles":            []string{"user"},
-			"x-hasura-user-id": "8cacd89f-9d0d-4035-a1a3-b1a338bef411",
+		var body struct {
+			Input struct {
+				Params model.Login `json:"params"`
+			}`json:"input"`
 		}
-		// set user specific claims data
-		claims.Metadata = metadata
-		claims.Email = "tester@gmail.com"
-		claims.LoginMethod = "regular_login"
-		claims.TokenType = "access_token"
-		claims.Subject = "4567890987654567-567gau90909-09RAS89"
 
-		accessToken, err := jwt_jwt.Sign(claims)
+		if err = ctx.BindJSON(&body); err != nil {
+			ctx.JSON(http.StatusBadRequest, model.Response{Message: "unprocessed_request", Success: false})
+			return
+		}
+
+		trim_phone := strings.TrimSpace(body.Input.Params.Username)
+
+		filters := []string{
+			fmt.Sprintf(`phone_number:{_eq: "%s"}`, trim_phone),
+		}
+
+		user, err := queries.FindUser(filters)
+		// Compare password
+		if err != nil {
+			logrus.Error("find user error", err)
+			ctx.JSON(http.StatusBadRequest, model.Response{Message: "invalid_credentials", Success: false})
+			return
+		}
+		// logrus.Info("User result --- ", user)
+		isMatch := utils.CompareHashedPassword(user.Password, body.Input.Params.Password)
+		if !isMatch {
+			ctx.JSON(http.StatusBadRequest, model.Response{Message: "invalid_credentials", Success: false})
+			return
+		}
+		err = CheckUserValidity(user)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, model.Response{Message: err.Error(), Success: false})
+			return
+		}
+	
+		// HERE CHECK IF THE USER ENABLE 2FA AUTHENTICATION 
+		// ..... 'LL IMPLEMENT IN RECENT FUTURE
+		if user.Enable2FA {
+			err = Send2faAuthOTP(user)
+			if err != nil {
+				logrus.Error("2fa sending error -- ", err)
+				ctx.JSON(http.StatusBadRequest, model.Response{Message: "2fa_error_encountered", Success: false})
+				return
+			}
+			ctx.JSON(http.StatusOK,gin.H{"message": "Authentication code sent via your phone number."})
+			return
+		}
+
+		accessToken, err := BuildToken(user)
 		if err != nil {
 			ctx.JSON(http.StatusInternalServerError, model.Response{
 				Message: "Failed to sign the token...",
@@ -35,18 +77,89 @@ func LoginHandler() gin.HandlerFunc {
 			})
 			return
 		}
-		var m_body = model.TwilioBody{
-			To:      "+251918492083",
-			Message: "Hello -- ",
-		}
-		err = services.TwilioSendSMS(m_body)
-		if err != nil {
-			logrus.Error("SOmething went wrong in twilio", err.Error())
-		}
 
-		ctx.JSON(http.StatusOK, model.Response{
-			Message: accessToken,
-			Success: true,
+		ctx.JSON(http.StatusOK, gin.H{
+			"access_token": accessToken,
 		})
 	}
+}
+
+func CheckUserValidity(user model.User) error {
+		if !user.Active {
+			return errors.New("disabled_or_inactive_account")
+		}
+		if((user.Email != nil && !user.IsEmailConfirmed) || (user.PhoneNumber != "" && !user.IsPhoneConfirmed)) {
+			return errors.New("unverified_account")
+		}
+		return nil
+}
+
+// SEND 2FA AUTH OTP - CODE
+func Send2faAuthOTP(user model.User) error {
+	generatedOTP := utils.GenerateOTP(6)
+
+	// insert otp
+	otp_object := model.OTP {
+		UserID: user.ID,
+		Code: generatedOTP,
+		Type: "authentication",
+	}
+	
+	 err := mutations.InsertOTP(otp_object)
+	if err != nil {
+		logrus.Error("Error on otp mutation", err)
+		return err
+	}
+	
+	// send opt via phone
+	// var m_body = model.TwilioBody{
+	// 	To:     strings.TrimSpace(user.PhoneNumber),
+	// 	Message: fmt.Sprintf(`%v - Is your 2FA authentication code and valid for only 20 minutes.`, generatedOTP),
+	// }
+	// err = services.TwilioSendSMS(m_body)
+	// if err != nil {
+	// 	logrus.Error("Something went wrong in twilio - ", err.Error())
+	// 	return err
+	// }
+
+	return nil
+}
+
+// Build accessToken
+
+func BuildToken(user model.User) (string, error) {
+	allowed_roles := []string{"user"}
+	if len(user.UserRoles) > 0 {
+	   for _, val := range user.UserRoles {
+		   allowed_roles = append(allowed_roles, val.Role.Name)
+	   }
+	}
+
+   var claims = &jwt_jwt.JWTClaims{}
+   metadata := map[string]interface{}{
+	   "roles":           allowed_roles,
+	   "x-hasura-user-id": user.ID,
+   }
+   // set user specific claims data
+   var user_email string
+   if user.Email != nil {
+	   user_email = *user.Email
+   }
+   claims.Metadata = metadata
+   claims.Email = user_email
+   // claims.LoginMethod = "regular_login"
+   claims.TokenType = "access_token"
+   claims.Subject = user.ID
+   claims.First_name = user.FirstName
+   claims.Last_name = user.LastName
+   claims.Middle_name = user.MiddleName
+   claims.SignUpMethod = user.SignUpMethod
+   claims.Gender = user.Gender
+
+   // HERE SIGN THE TOKEN
+   accessToken, err := jwt_jwt.Sign(claims)
+   if err != nil {
+	return "", err
+   }
+   return accessToken, nil
 }
--- api/handlers/auth/signup.go
+++ api/handlers/auth/signup.go
@@ -1,14 +1,110 @@
 package auth
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"strings"
 
+	"github.com/Ethiopian-Education/edu-auth-server.git/config"
+	"github.com/Ethiopian-Education/edu-auth-server.git/graph"
 	"github.com/Ethiopian-Education/edu-auth-server.git/model"
+	"github.com/Ethiopian-Education/edu-auth-server.git/utils"
+	"github.com/Ethiopian-Education/edu-auth-server.git/utils/services"
 	"github.com/gin-gonic/gin"
+	"github.com/hasura/go-graphql-client"
+	"github.com/sirupsen/logrus"
 )
-
+type user_users_insert_input struct {
+	Email                 *string      `json:"email" graphql:"email,omitempty"`
+	PhoneNumber           string      `json:"phone_number" graphql:"phone_number"`
+	FirstName             string      `json:"first_name" graphql:"first_name"`
+	MiddleName            string      `json:"middle_name" graphql:"middle_name"`
+	LastName              string      `json:"last_name" graphql:"last_name,omitempty"`
+	Username  string `json:"username,omitempty"`
+	Password string `json:"password"`
+	Gender string `json:"gender"`
+	SignupMethod string `json:"signup_method"`
+	OTPS struct {
+		Data struct {
+			Code string `json:"code" graphql:"code"`
+			Type string `json:"type" graphql:"type"`
+		} `json:"data" graphql:"data"`
+	} `json:"otps" graphql:"otps"`
+}
 func SignUpHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, model.Response{Message: "Successfully signed up", Success: true})
+		var err error
+		var body struct {
+			Input struct {
+				Params model.Signup `json:"params"`
+			}`json:"input"`
+		}
+
+		if err = ctx.BindJSON(&body); err != nil {
+			logrus.Errorf(`Error encountered when decoding signup req body : %v`, err)
+			ctx.JSON(http.StatusBadRequest, model.Response{Message: "unprocessed_request", Success: false})
+			return
+		}
+
+		// logrus.Info("Sighup creds : ", body)
+
+		hasedPassword, err := utils.HashPassword(body.Input.Params.Password)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, model.Response{Message: "Bad request", Success: false})
+		}
+		trim_phone := strings.TrimSpace(body.Input.Params.PhoneNumber)
+
+		generated_opt := utils.GenerateOTP(6)
+		// logrus.Info("Generated otp : --- ", generated_opt)
+
+		object := user_users_insert_input{
+			FirstName: body.Input.Params.FirstName,
+			MiddleName: body.Input.Params.MiddleName,
+			LastName: body.Input.Params.LastName,
+			Email: body.Input.Params.Email,
+			Gender: body.Input.Params.Gender,
+			Password: hasedPassword,
+			PhoneNumber: trim_phone,
+			SignupMethod: "regular_signup",
+		}
+		object.OTPS.Data.Code = generated_opt
+		object.OTPS.Data.Type = "phone_verification"
+
+		var mutation struct {
+			InserUser model.User `graphql:"insert_user_user(object:$object)"`
+		}
+		variables := map[string]interface{}{
+			"object": object,
+		}
+
+		client := &http.Client{
+			Transport: &graph.Transport{T: http.DefaultTransport},
+		}
+
+		graph_client := graphql.NewClient(config.HASURA_GRAPHQL_URL, client)
+
+		if err = graph_client.Mutate(context.Background(), &mutation, variables); err != nil {
+			logrus.Error("Mutation error : ", err.Error())
+			ctx.JSON(http.StatusBadRequest, model.Response{Message: "user_signup_mutation_error" , Success: false})
+			return
+		}
+
+		// logrus.Infoln("User Added -- ", mutation.InserUser)
+		if mutation.InserUser.Email != nil {
+			logrus.Infoln("send otp via Email")
+		}
+		
+		// Send OTP via phone...
+		var m_body = model.TwilioBody{
+			To:     trim_phone,
+			Message: fmt.Sprintf(`%v - Is your confirmation code and valid for only 20 minutes, please confirm to activate your account.`, generated_opt),
+		}
+		err = services.TwilioSendSMS(m_body)
+		if err != nil {
+			logrus.Error("Something went wrong in twilio - ", err.Error())
+		}
+
+		ctx.JSON(http.StatusOK, model.Response{Message: fmt.Sprintf("Successful user id - %s", mutation.InserUser.ID), Success: true})
 	}
 }
