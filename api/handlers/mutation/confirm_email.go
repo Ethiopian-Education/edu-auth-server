4ff92fc71f43527d6300b0e36e517a7e4565f777
Minilik Shumete <minilikshumete@gmail.com>
2023-10-27 00:45:54 +0300

    email-confirm-mutation

---
+++ api/handlers/mutation/confirm_email.go
@@ -0,0 +1,33 @@
+package mutations
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/Ethiopian-Education/edu-auth-server.git/config"
+	"github.com/Ethiopian-Education/edu-auth-server.git/graph"
+	"github.com/hasura/go-graphql-client"
+)
+
+func VerifyEmail(userId string) error {
+	var err error
+	var mutation struct {
+		UpdateUserUser struct {
+			ID string `json:"id" graphql:"id"`
+		} `graphql:"update_user_user(pk_columns:{id: $id}, _set: {is_email_confirmed:true})"`
+	}
+
+	client := &http.Client{
+		Transport: &graph.Transport{T: http.DefaultTransport},
+	}
+
+	graph_client := graphql.NewClient(config.HASURA_GRAPHQL_URL, client)
+
+    err = graph_client.Mutate(context.Background(), &mutation, map[string]interface{}{
+		"id": userId,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
